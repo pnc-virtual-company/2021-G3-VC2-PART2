9becf3c90b276ff7e70961b24c1df81cd6b24d2d
Sothoun-Phauv <sothoun.phauv@student.passerellesnumeriques.org>
2021-12-17 11:07:31 +0700

    #7 create menu bar

---
+++ front/src/components/nav/Navigation.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-list nav dense>
+    <v-list-item-group active-class="deep-purple--text text--accent-4">
+        <v-list-item class="d-logo">
+            <v-avatar size="90px" >
+                <img alt="Avatar" src="../../assets/logo.svg">
+            </v-avatar>
+        </v-list-item>
+        <v-list-item >
+            <h3>SOTHOUN PHAUV</h3><br>
+            
+        </v-list-item>
+        <v-list-item>
+            <h4>Administrator</h4>
+        </v-list-item>
+        <v-list-item 
+         v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path"
+        >
+            <v-list-item-icon>
+                <v-icon>{{item.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+        </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+             menuItems: [
+                        { title: 'Users', path: '/user', icon: 'mdi-account-multiple-plus' },
+                        { title: 'Students', path: '/student', icon: 'mdi-account-box' },
+                        { title: 'Permission', path: '/permission', icon: 'mdi-account-key' },
+                        { title: 'Discipline', path: '/discipline', icon: 'mdi-account-star' },
+                ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.d-logo{
+    display: flex;
+    justify-content: center;
+}
+h3{
+    text-align: center;
+}
+
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,9 +3,31 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+
 const routes = [
-  
+
+{
+  path: '/user',
+  name: 'Users',
+  component: () => import('@/components/views/Users.vue')
+},
+{
+  path: '/student',
+  name: 'Students',
+  component: () => import('@/components/views/Student.vue')
+},
+{
+  path: '/permission',
+  name: 'Permission',
+  component: () => import('@/components/views/Permission.vue')
+},
+{
+  path: '/discipline',
+  name: 'Discipline',
+  component: () => import('@/components/views/Discipline.vue')
+},
 ]
+
 
 const router = new VueRouter({
   mode: 'history',
