c8c6e2fce50e11b901e4bbb8d33828df79380e5e
Sreynot <sreynot.bou@student.passerellesnumeriques.org>
2021-12-17 13:49:22 +0700

    #5 create signin/signout in back-end

---
+++ back/app/Http/Controllers/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+        
+    }
+    //
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email= $request->email;
+        $user->password = bcrypt($request->password);
+        $user->profile= $request->profile;
+        $user->save();
+        return response()->json(['Message' =>' Create user Succesfully'], 201);
+    }
+
+// .............................User signin.........................
+    public function signin(Request $request)
+    {
+        //Chek password
+        $user = User::where ('email', $request->email)->first();
+
+        //Create user
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json(['message' => 'User can not lognin'], 401);
+        }
+        return response()->json([
+            'User signin succesfuly'
+        ]);
+    }
+
+    // ........................User signout........................
+    public function signout(Request $request)
+    {
+        return response()->json(['message'=>'User signout successfuly']);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//....................get User.............................
+
+Route::get('/users', [UserController::class, 'index']);
+
+// ..................create User ..........................
+Route::post('/users', [UserController::class, 'store']);
+
+//....................User Signin............................
+
+Route::post('/signin', [UserController::class, 'signin']);
+
+// ................User signout ........................
+Route::post('/signout',[UserController::class, 'signout']); 
+
+
+
