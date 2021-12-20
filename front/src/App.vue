98a4c64900c696c9a8c28522a5a849100b620369
SomounOm <somoun.om077@gmail.com>
2021-12-20 09:41:12 +0700

    #00

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,49 +1,22 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="showDrawer" absolute bottom temporary>
-      <app-nav></app-nav>
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon
-        @click.stop="showDrawer = !showDrawer"
-      ></v-app-bar-nav-icon>
-      <v-avatar size="50px" class=" ml-3 mr-3">
-          <img alt="Avatar" src="assets/pnc.png">
-      </v-avatar>
-      <v-toolbar-title>
-          <h2>Student Life</h2>
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-exit-to-app</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Navbar/>
     <v-main>
-      <router-view class="pa-12"></router-view>
+      <router-view/>
     </v-main>
-
-    <v-footer app> </v-footer>
   </v-app>
 </template>
 
 <script>
-import Navigation from "./components/nav/Navigation.vue";
-
+import Navbar from '@/components/nav/Navigation';
 export default {
-  name: "App",
   components: {
-    "app-nav": Navigation,
+    Navbar
   },
-
-  data() {
+  data(){
     return {
-      showDrawer: false,
-    };
+
+    }
   },
 };
 </script>
- 
--- front/src/components/nav/Navigation.vue
+++ front/src/components/nav/Navigation.vue
@@ -1,54 +1,75 @@
 <template>
-  <v-list nav dense>
-    <v-list-item-group active-class="deep-purple--text text--accent-4">
-        <v-list-item class="d-logo">
-            <v-avatar size="90px" >
-                <img alt="Avatar" src="../../assets/logo.svg">
-            </v-avatar>
-        </v-list-item>
-        <v-list-item >
-            <h3>SOTHOUN PHAUV</h3><br>
-            
-        </v-list-item>
-        <v-list-item>
-            <h4>Administrator</h4>
-        </v-list-item>
-        <v-list-item 
-         v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path"
+  <nav>
+    <v-navigation-drawer v-model="sidebar" app absolute left temporary>
+      <v-list>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
         >
-            <v-list-item-icon>
-                <v-icon>{{item.icon}}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{item.title}}</v-list-item-title>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
         </v-list-item>
-    </v-list-item-group>
-  </v-list>
+          </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar >
+      <span class="hidden-sm-and-up">
+        <v-app-bar-nav-icon @click="sidebar = !sidebar">
+        </v-app-bar-nav-icon>
+      </span>
+       <img
+            src="../../assets/pnc.png"
+            alt="Vuetify Admin"
+            width="50"
+            height="50"
+        />
+      <v-toolbar-title class="ml-2">
+       
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          active-class="grey-5 yellow--text" 
+          text
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-icon left class="orange--text">{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items> 
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-             menuItems: [
-                        { title: 'Users', path: '/user', icon: 'mdi-account-multiple-plus' },
-                        { title: 'Students', path: '/student', icon: 'mdi-account-box' },
-                        { title: 'Permission', path: '/permission', icon: 'mdi-account-key' },
-                        { title: 'Discipline', path: '/discipline', icon: 'mdi-account-star' },
-                ]
-        }
+  data() {
+    return {
+        appTitle: 'PNC Cambodia',
+        sidebar: false,
+        group: false,
+        menuItems: [
+            { title: 'Users', path: '/user', icon: 'mdi-account-multiple-plus' },
+            { title: 'Students', path: '/student', icon: 'mdi-account-box' },
+            { title: 'Permission', path: '/permission', icon: 'mdi-account-key' },
+            { title: 'Discipline', path: '/discipline', icon: 'mdi-account-star' },
+        ]
     }
+  }
 }
 </script>
 
-<style scoped>
-.d-logo{
-    display: flex;
-    justify-content: center;
-}
-h3{
-    text-align: center;
-}
+<style>
 
 </style>
