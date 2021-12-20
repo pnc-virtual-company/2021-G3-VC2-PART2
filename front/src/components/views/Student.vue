0c50320d7bda2c823654da4f5bebceef9fff8b74
SomounOm <somoun.om077@gmail.com>
2021-12-20 08:08:22 +0700

    #00

---
+++ front/src/components/views/Student.vue
@@ -0,0 +1,48 @@
+<template>
+  
+ <v-card
+   
+    max-width="1000"
+    class="mx-auto mt-4"
+    
+  >  
+    <v-list >
+      <v-list-item >
+        <v-list-item-avatar>
+          <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+          <v-list-item-subtitle>Author</v-list-item-subtitle>
+        </v-list-item-content>
+        <div>
+            <v-text class="mr-2">Tell:090488985</v-text>
+            <v-text class="m-3">Email: somoun@gmail.com</v-text>
+        </div>
+       
+        <div>
+            <v-btn class="mr-2" color='red' prepend-icon>
+                delete
+            </v-btn>
+            <v-btn color='blue' class="mr-3">
+                update
+            </v-btn>
+        </div>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .container{
+        margin-top: 100px;
+        
+    }
+
+</style>
