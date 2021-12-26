6a3df81d6f74421b02d4ef777d603896e0105adb
nimout mang <nimout.mang@student.passerellesnumeriques.org>
2021-12-26 16:37:58 +0700

    #00

--- front/src/components/views/Permission.vue
+++ front/src/components/views/Permission.vue
@@ -1,10 +1,44 @@
 <template>
-  <h1>Permission</h1>
+  <section>
+    <Formpermission @add-permission="Createpermission"></Formpermission>
+    <Cardpermission></Cardpermission>
+    
+    </section>
 </template>
 
 <script>
-export default {};
+import Cardpermission from '../../components/page/permission/Cardpermission.vue';
+import Formpermission from '../../components/page/permission/Formpermission.vue'
+import axios from "axios"
+const APP_URL = 'http://127.0.0.1:8000/api';
+export default {
+  components:{
+    Cardpermission,
+    Formpermission
+  },
+  data(){
+    return{
+      studentPermission:[]
+    }
+  },
+  methods: {
+    getPermission(){
+      axios.get(APP_URL + '/permission').then(res=>{
+        this.studentPermission = res.data;
+        console.log(this.studentPermission);
+      })
+    },
+    Createpermission(newPermission){
+      axios.post(APP_URL + '/permission',newPermission).then(res =>{
+        console.log(res.data);
+      })
+    }
+  },
+  mounted() {
+    this.getPermission();
+  },
+};
 </script>
 
-<style>
+<style scoped>
 </style>
